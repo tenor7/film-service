29691031cb072fff585d5f8c0d760ba718d216ca
tenor <mariyaivanova2001@gmail.com>
2022-12-19 12:26:24 +0300

    initial commit

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,13 +5,13 @@ api = 'http://localhost:8081/film'
 
 def test_film_empty_get():
     s = requests.Session()
-    id = '8c34c25b-b4a1-477a-a938-d6ad10442c98'
+    id = 'c1827b19-0b09-4b62-afbc-c64bf237e70b'
     response = s.get(f'{api}/{id}')
     assert response.status_code == 200
 
 def test_film_save_and_get():
     s = requests.Session()
-    parameters = "?title=tenor&authorId=8c34c25b-b4a1-477a-a938-d6ad10442c98"
+    parameters = "?title=tenor&authorId=c1827b19-0b09-4b62-afbc-c64bf237e70b"
     response = s.post(f'{api}/{parameters}')
     assert response.status_code == 200
     assert response.json().get('title') == 'tenor'
