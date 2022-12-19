23a052424a3f1b1d4f5ac1487c93ec8cea3dcbc3
tenor <mariyaivanova2001@gmail.com>
2022-12-19 13:20:40 +0300

    fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,19 +3,20 @@ import requests
 api = 'http://localhost:8081/film'
 
 
-def test_film_empty_get():
+
+def test_user_empty_get():
     s = requests.Session()
     id = 'c1827b19-0b09-4b62-afbc-c64bf237e70b'
-    response = s.get(f'{api}/{id}')
+    response = s.get(f'{api}/find/{id}')
     assert response.status_code == 200
 
-def test_film_save_and_get():
+def test_user_save_and_get():
     s = requests.Session()
     parameters = "?title=tenor&authorId=c1827b19-0b09-4b62-afbc-c64bf237e70b"
-    response = s.post(f'{api}/{parameters}')
+    response = s.post(f'{api}/add/{parameters}')
     assert response.status_code == 200
     assert response.json().get('title') == 'tenor'
     createdFilmId = response.json().get('id')
-    responseFilm = s.get(f'{api}/{createdFilmId}')
+    responseFilm = s.get(f'{api}/find/{createdFilmId}')
     assert responseFilm.status_code == 200
     assert responseFilm.json().get('title') == 'tenor'
